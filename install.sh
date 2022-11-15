44748c154d40230620c4220dc00f2c3272d5e437
AnjuMeleth <46922680+AnjuMeleth@users.noreply.github.com>
2022-11-15 16:58:07 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,33 @@
-#!/bin/bash
+#!/bin/sh
 #Installing Nagios Core 4.4.6 in Ubuntu 20.x
 sudo apt-get update
 sudo apt-get install -y autoconf gcc libc6 make wget unzip apache2 php \
   libapache2-mod-php7.4 libgd-dev
+sudo apt-get install -y openssl libssl-dev
+cd /tmp
+wget -O nagioscore.tar.gz https://github.com/NagiosEnterprises/nagioscore/archive/nagios-4.4.6.tar.gz
+tar xzf nagioscore.tar.gz
+cd /tmp/nagioscore-nagios-4.4.6/
+sudo ./configure --with-httpd-conf=/etc/apache2/sites-enabled
+sudo make all
+sudo make install-groups-users
+sudo usermod -a -G nagios www-data
+sudo make install
+sudo make install-daemoninit
+sudo make install-commandmode
+sudo make install-config
+sudo make install-webconf
+sudo a2enmod rewrite
+sudo a2enmod cgi
+sudo ufw allow Apache
+sudo ufw reload
+sudo htpasswd -c /usr/local/nagios/etc/htpasswd.users nagiosadmin
+sudo systemctl restart apache2.service
+sudo systemctl start nagios.service
+sudo systemctl status nagios.service
+#Installing Nagios Core 4.4.6 in Ubuntu 20.x
+sudo apt-get update
+sudo apt-get install -y autoconf gcc libc6 make wget unzip apache2 php libapache2-mod-php7.4 libgd-dev
 sudo apt-get install -y openssl libssl-dev
 cd /tmp
 wget -O nagioscore.tar.gz https://github.com/NagiosEnterprises/nagioscore/archive/nagios-4.4.6.tar.gz
