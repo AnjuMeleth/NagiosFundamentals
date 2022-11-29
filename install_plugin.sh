1f3619f0a77ea1e00e85131267fc0b36193e910c
AnjuMeleth <46922680+AnjuMeleth@users.noreply.github.com>
2022-11-29 11:07:22 +0530

    Update install_plugin.sh

--- install_plugin.sh
+++ install_plugin.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sudo apt-get update
-sudo apt-get install -y autoconf gcc libc6 libmcrypt-dev make \
+apt-get update
+apt-get install -y autoconf gcc libc6 libmcrypt-dev make \
 	libssl-dev wget bc gawk dc build-essential snmp libnet-snmp-perl \
 	gettext
 cd /tmp
@@ -8,7 +8,7 @@ wget --no-check-certificate -O nagios-plugins.tar.gz \
 https://github.com/nagios-plugins/nagios-plugins/archive/release-2.3.3.tar.gz
 tar zxf nagios-plugins.tar.gz
 cd /tmp/nagios-plugins-release-2.3.3/
-sudo ./tools/setup
-sudo ./configure 
-sudo make
-sudo make install
+./tools/setup
+./configure 
+make
+make install
