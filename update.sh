396bea2af0d0a136570897b8ac489bd4aa976c4c
AnjuMeleth <46922680+AnjuMeleth@users.noreply.github.com>
2022-12-06 06:23:54 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,12 +6,3 @@ cd /tmp/nagioscore-nagios-4.4.9/
 ./configure --with-httpd-conf=/etc/apache2/sites-enabled
 make all
 make install
-make install-daemoninit
-make install-commandmode
-make install-config
-make install-webconf
-a2enmod rewrite
-a2enmod cgi
-systemctl restart apache2.service
-systemctl start nagios.service
-systemctl status nagios.service
