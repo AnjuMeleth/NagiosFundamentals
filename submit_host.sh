929ebb2e21f8c60dd0cacbce5fba0a5ff8593768
AnjuMeleth <46922680+AnjuMeleth@users.noreply.github.com>
2023-01-26 08:00:01 +0530

    Update submit_host.sh

--- submit_host.sh
+++ submit_host.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-NOW=`date +%s` 
-HOST=$1 
-STATUS=$2 
-OUTPUT=$3 
-echo "[$NOW] PROCESS_HOST_CHECK_RESULT;$HOST;$STATUS;$OUTPUT" \ 
-      >/usr/local/nagios/var/nagios/rw/nagios.cmd 
-exit 0 
+NOW=`date +%s`
+HOST=$1
+STATUS=$2
+OUTPUT=$3
+COMMAND_FILE='/usr/local/nagios/var/rw/nagios.cmd'
+echo "[$NOW] PROCESS_HOST_CHECK_RESULT;$HOST;$STATUS;$OUTPUT" > $COMMAND_FILE
+exit 0
